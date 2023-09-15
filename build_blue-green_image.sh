f117f8fccebfc904b09313d03c21952c80656911
bla <bla@mail.com>
2023-09-15 14:18:39 +0100

    2nd commit; 1st push to remote

--- build_blue-green_image.sh
+++ build_blue-green_image.sh
@@ -1,30 +1,32 @@
 #!/usr/bin/env sh
 
-### Build Image for Green-Blue K8s Deployment Practice ###
-#       deployment shows the container name              #
-#                   Eitan CJ 2023                        #
-###                 Shell Script                       ###
+### Build Image for Blue-Green K8s Deployment ###
+#     nginx shows ctr hostname in blue/green    #
+#                  EitanCJ 2023                 #
+#                  Shell Script                 #
+### ----------------------------------------- ###
 
-# vars
-sedscript='8-replace-with-ctrname.sh'
+# constants
+sedscript='8-format-html-file.sh'
 htmlfile='/usr/share/nginx/html/index.html'
-newimg='nginx:ctrname'
+newimg='nginx:bluegreen'
 
-# build static html file
+# build custom static html-file
 cat <<EOF >index.html
 <!DOCTYPE html>
 <title>envclr</title>
 <html>
-<body>
+<body style="background-color:#FFFFFF;text-align: center;
+margin-right: auto; margin-left: auto;margin-top:50px">
 
-<h1>Container Hostname:</h1>
-<h2> CtrHname</h2>
+<h3>Container Hostname</h3>
+<p style="font-size: 50px;">CtrHname</h2>
 
 </body>
 </html>
 EOF
 
-# create script to update html file so that it displays the ctr name assigned at startup
+# create script to make the html file display the hostname assiged to the ctr at startup
 cat <<EOF >$sedscript
 #!/bin/sh
 
@@ -52,26 +54,27 @@ EOF
 
 # create Dockerfile
 cat <<EOF >Dockerfile
-FROM nginx:latest
+FROM nginx:1.25.2-alpine3.18-slim
+LABEL maintainer=eitancj
 
 # set vars
 ENV INDEX=$htmlfile
 ENV ScriptDir=/docker-entrypoint.d/
 ENV SedScript=$sedscript
 
-# place custom static-html file for nginx to pick up
+# place custom static html-file for nginx to pick up
 COPY index.html \$INDEX
 
-# place script that updates the html file with the ctr's name in nginx startup-scripts dir
+# place script that updates the html-file in nginx's startup-scripts dir
 COPY \$SedScript \$ScriptDir\$SedScript
 RUN chmod u+x \$ScriptDir\$SedScript
 EOF
 
-# build image
-docker image build -q -t $newimg . > /dev/null
+# build image quietly
+docker image build -q -t $newimg . >/dev/null
 
 # show image
 docker images $newimg
 
 # clean up
-rm $sedscript index.html Dockerfile 
+rm $sedscript index.html Dockerfile
